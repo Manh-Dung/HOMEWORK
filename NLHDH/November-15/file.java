47c694d39fe73878d97b19bb524a08f11ff5ece2
Manh Dung <manhdung5a@gmail.com>
2021-11-20 14:37:10 +0700

    Screen Record

--- NLHDH/November-15/file.java
+++ NLHDH/November-15/file.java
@@ -1,22 +1,9 @@
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class file {
+ 
     public static void main(String[] args) {
-        String fol = "C:\\Users\\dung5\\OneDrive\\HOMEWORK\\NLHDH\\November-15";
-
-        try {
-            List<File> files = Files.list(Paths.get(fol)).filter(Files::isRegularFile).map(Path::toFile)
-                    .collect(Collectors.toList());
-                    
-            files.forEach(System.out::println);
-        } catch (IOException e) {
-            // Error while reading the directory
-        }
+        int count = new File("C:\\Users\\dung5\\OneDrive\\HOMEWORK\\NLHDH\\November-15").list().length;
+        System.out.println("Number of file : " + count);
     }
 }
+// Dem so luong flie va folder con trong folder cha
--- NLHDH/November-15/file.py
+++
@@ -1,4 +0,0 @@
-import os
-
-path, dirs, files = next(os.walk("C:\Users\dung5\OneDrive\HOMEWORK\NLHDH"))
-file_count = len(files)

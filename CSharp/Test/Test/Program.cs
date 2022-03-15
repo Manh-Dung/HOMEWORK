59530348569c2fcde10a9d89f15cb3b94ef6f100
Manh Dung <manhdung5a@gmail.com>
2022-03-15 21:30:02 +0700

    Test Visual

---
+++ CSharp/Test/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        class test
+        {
+            private double diem;
+            public double Diem
+            {
+                get { return diem; }
+                set { diem = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            test t = new test();
+            t.Diem = 10;
+            Console.WriteLine(t.Diem);
+            Console.ReadLine();
+        }
+    }
+}

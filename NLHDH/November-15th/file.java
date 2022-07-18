4743ff269e7d6d851bf257b02b887c9dc20684fe
Manh-Dung <manhdung5a@gmail.com>
2022-07-18 18:29:13 +0700

    change

--- NLHDH/November-15th/file.java
+++ NLHDH/November-15th/file.java
@@ -2,7 +2,7 @@ import java.io.File;
 public class file {
  
     public static void main(String[] args) {
-        int count = new File("C:\\Users\\dung5\\OneDrive\\HOMEWORK\\NLHDH\\November-15").list().length;
+        int count = new File("C:\\Users\\dung5\\OneDrive\\HOMEWORK\\NLHDH\\November-15th").list().length;
         System.out.println("Number of file : " + count);
     }
 }
